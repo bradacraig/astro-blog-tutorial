97d1d568dac87aa8182512d2658aaa50b8e723fb
Brad Craig <brad.a.craig@gmail.com>
2024-09-11 14:58:09 +1200

    updated config with netlify address

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   publicDir: 'public',
+  site: 'https://astro-blog-tutorial-bc.netlify.app'
 });
