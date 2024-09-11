5456d7df0f83881c277fac9d666898bb0701c435
Brad Craig <brad.a.craig@gmail.com>
2024-09-11 16:23:56 +1200

    added greeting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
   publicDir: 'public',
-  site: 'https://astro-blog-tutorial-bc.netlify.app'
+  site: 'https://astro-blog-tutorial-bc.netlify.app',
+  integrations: [preact()]
 });
